81b3448800f331412838bc22281675706e2afef5
Jaki <mrjuoss@gmail.com>
2020-04-21 10:20:00 +0700

    Learn Route Wildcard adn Pass Request Data to View

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,24 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('/test', function () {
+        $name = request('name');
+        //return "test ".$name;
+        return view('test', ['name' => $name]);
+    });
+    Route::get('/post/{title}', function ($title) {
+        $posts = [
+            'my-first-post' => 'Hello, this is my first blog post',
+            'my-second-post' => 'Now I am getting the hang of thus blogging thing'
+        ];
+        if (! array_key_exists($title, $posts))
+        {
+            abort(404, 'Sorry, that post was not fpound');
+        }
+        return view ('post', [
+           'post' => $posts[$title] /** OR add Ternry -> ?? 'Nothing here yet' */
+        ]);
+    });
     Route::get('/dashboard', 'HomeController@index')->name('home');
     Route::get('produk', 'ProdukController@index');
     Route::get('produk/tambah', 'ProdukController@tambah');
