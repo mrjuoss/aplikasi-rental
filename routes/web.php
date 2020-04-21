b4e89903957ee83191a519dd0bafe05cc6b9bead
Jaki <mrjuoss@gmail.com>
2020-04-21 12:32:23 +0700

    Learn Routing to Controller

--- routes/web.php
+++ routes/web.php
@@ -24,19 +24,22 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
         //return "test ".$name;
         return view('test', ['name' => $name]);
     });
-    Route::get('/post/{title}', function ($title) {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post',
-            'my-second-post' => 'Now I am getting the hang of thus blogging thing'
-        ];
-        if (! array_key_exists($title, $posts))
-        {
-            abort(404, 'Sorry, that post was not fpound');
-        }
-        return view ('post', [
-           'post' => $posts[$title] /** OR add Ternry -> ?? 'Nothing here yet' */
-        ]);
-    });
+    Route::get('/post/{title}', 'PostController@show');
+
+//    Route::get('/post/{title}', function ($title) {
+//        $posts = [
+//            'my-first-post' => 'Hello, this is my first blog post',
+//            'my-second-post' => 'Now I am getting the hang of thus blogging thing'
+//        ];
+//        if (! array_key_exists($title, $posts))
+//        {
+//            abort(404, 'Sorry, that post was not found');
+//        }
+//        return view ('post', [
+//           'post' => $posts[$title] /** OR add Ternry -> ?? 'Nothing here yet' */
+//        ]);
+//    });
+
     Route::get('/dashboard', 'HomeController@index')->name('home');
     Route::get('produk', 'ProdukController@index');
     Route::get('produk/tambah', 'ProdukController@tambah');
